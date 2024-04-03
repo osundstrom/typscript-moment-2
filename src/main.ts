331135b8e964c6157005935084b246d2dc85c712
Oskar Sundström <ossu2300@student.miun.se>
2024-04-03 19:35:46 +0200

    påbörjat addTodo, kollar att  priority ärt mellan 1 o 3 samt task ej är tomt, skapar todo array.

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,54 @@
 
 
-//Enum med de 3 olika viktighetsgraderna
-enum Priority {
-    high = 1,
-    medium = 2,
-    low = 3
-}
-
-
 //Interface Todo.
 interface Todo {
-    task: string;
+    task: string; 
     completed: boolean;
-    priority: Priority;
-}
+    priority: number;
+};
+
+
+//----------------------------------------------------------------------------------//
+//----------------------------------------------------------------------------------//
+//----------------------------------------------------------------------------------//
+//----------------------------------------------------------------------------------//
+
+const addTodo =document.getElementById("addTodo") as HTMLFormElement;
+const taskInput:HTMLInputElement = document.getElementById("task") as HTMLInputElement;
+const priorityInput:HTMLInputElement = document.getElementById("priority") as HTMLInputElement;
+
+//----------------------------------------------------------------------------------//
+//----------------------------------------------------------------------------------//
+//----------------------------------------------------------------------------------//
+//----------------------------------------------------------------------------------//
+
+class TodoList {
+
+    todos: Todo[];
+
+addTodo(task: string, priority: number): boolean { 
+    if (priority < 1) {//om prority är  mindre än 1, false
+        console.log("false")
+        return false; 
+        }
+    else if(priority > 3) {//om prority är  större än 3, false
+        console.log("false")
+            return false;
+        }
+
+    else if(task == "") {//Om task är tomt, false
+        console.log("false")
+        return false;
+        
+    }
+
+
+}}
+
+
+
+
+
+
 
 
